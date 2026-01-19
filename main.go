87fe64729937f89af5d3c57e2d892451b42e67ef
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2026-01-20 02:47:55 +0600

    Task: data inserted on database successfully

--- main.go
+++ main.go
@@ -1,33 +1,135 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/gocql/gocql"
 )
 
+var BooksDataColumn = []string{
+	"isbn13",
+	"isbn10",
+	"title",
+	"subtitle",
+	"authors",
+	"categories",
+	"thumbnail",
+	"description",
+	"published_year",
+	"average_rating",
+	"num_pages",
+	"ratings_count",
+}
+var BookDbColumn = []string{
+	"isbn", //isbn10
+	"title",
+	"subtitle",
+	"authors",    // now keep it as list
+	"categories", // keep it as list
+	"thumbnail",
+	"description",
+	"published_year",
+	"average_rating",
+	"num_pages",
+	"ratings_count",
+}
 
-func main(){
+func insert_csv_data(session *gocql.Session) {
+	file, err := os.Open("./data/data.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
+	scanner := bufio.NewScanner(file)
+	isFirstLine := true
+	booksCount := 0
 
+	for scanner.Scan() {
+		line := scanner.Text()
+		if isFirstLine {
+			isFirstLine = false
+			continue
+		}
+
+		values := strings.Split(line, ",")
+		insertQuery := `
+		INSERT INTO books (
+			isbn,
+			title,
+			subtitle,
+			authors,
+			categories,
+			thumbnail,
+			description,
+			published_year,
+			average_rating,
+			num_pages,
+			ratings_count
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
+		authorList := strings.Split(values[4], " ")
+		categoryList := strings.Split(values[5], " ")
+		publishedYear, _ := strconv.Atoi(values[8])
+		avgRatingFloat64, _ := strconv.ParseFloat(values[9], 32)
+		numPages, _ := strconv.Atoi(values[10])
+		ratingCount, _ := strconv.Atoi(values[11])
+
+		avgRating := float32(avgRatingFloat64)
+
+		err = session.Query(insertQuery).Bind(
+			values[1],
+			values[2],
+			values[3],
+			authorList,
+			categoryList,
+			values[6],
+			values[7],
+			publishedYear,
+			avgRating,
+			numPages,
+			ratingCount,
+		).Exec()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		booksCount += 1
+	}
+	session.Close()
+	fmt.Println("Books Count: ", booksCount)
+
+}
+
+func init_db() {
 	// cluster config
 	cluster := gocql.NewCluster("127.0.0.1")
 	cluster.Port = 9042
 	cluster.Consistency = gocql.Quorum
 
-
-
 	// create session
 	session, err := cluster.CreateSession()
-	if err != nil{
+	if err != nil {
 		log.Fatal("Connection err: ", err)
 	}
 	defer session.Close()
 
+	// remove old keyspace
+	err = session.Query(`
+		DROP KEYSPACE IF EXISTS library_keyspace
+	`).Exec()
+	if err != nil {
+		log.Fatal("Cannot drop keyspace:", err)
+	}
+
 	// create keyspace
 	err = session.Query(`
-		Create keyspace if not exists test_keyspace
+		Create keyspace if not exists library_keyspace
 		with replication = {
 			'class': 'SimpleStrategy',
 			'replication_factor': 1
@@ -40,38 +142,40 @@ func main(){
 
 	session.Close()
 
-	cluster.Keyspace = "test_keyspace"
+	cluster.Keyspace = "library_keyspace"
 
 	session, err = cluster.CreateSession()
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	defer session.Close()
 
 	err = session.Query(`
-		Create table if not exists demo_table(
-			id UUID primary key,
-			name text
+		Create table if not exists books(
+			isbn text primary key,
+			title text,
+			subtitle text,
+			authors list<text>,
+			categories list<text>,
+			thumbnail text,
+			description text,
+			published_year int,
+			average_rating float,
+			num_pages int,
+			ratings_count int,
 		)
 	`).Exec()
 
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	id := gocql.TimeUUID()
-	err = session.Query(`INSERT INTO demo_table (id, name) values (?, ?)`, id, "Pramananda",).Exec()
-	if err != nil{
-		log.Fatal(err)
-	}
+	insert_csv_data(session)
 
-	var name string
-	err = session.Query(`select name from demo_table where id = ?`, id).Scan(&name)
+}
 
-	if err != nil{
-		log.Fatal(err)
-	}
+func main() {
 
+	init_db()
 
-	fmt.Println("User name: ", name)
 }
